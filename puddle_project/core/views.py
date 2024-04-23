9f255dd0c29350f930e44b63e7b76c9515597144
Dabira12 <43392188+Dabira12@users.noreply.github.com>
2024-04-23 13:48:57 -0400

    Delete puddle_project directory

--- puddle_project/core/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from wager.models import Sport, Wager
-# Create your views here.
-def index(request):
-    wagers = Wager.objects.all()
-    return render(request, 'core/index.html',{'wagers':wagers})
