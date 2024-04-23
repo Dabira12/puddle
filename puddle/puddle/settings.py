055ab4e1f8deda3ec16f6ce2971bfae4b078db27
Oluwadabira Akinwumi <dabira.akinwumi@gmail.com>
2024-04-23 14:28:37 -0400

    tryed somethign new

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -29,6 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "True"
 
+# DEBUG = "False"
+
+# ALLOWED_HOSTS = []
+
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
